4ccaecc4050137afce9eb83a96d474b479378385
kenny <89335272@qq.com>
2017-11-15 14:30:57 +0800

    demo
    Signed-off-by: kenny <89335272@qq.com>

--- README.md
+++ README.md
@@ -18,4 +18,41 @@ npm run build
 npm run build --report
 ```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+vue.js2.X 微信开发的DEMO
+
+
+.
+├── build/                      # webpack config files
+│   └── ...
+├── config/
+│   ├── index.js                # main project config
+│   └── ...
+├── notes/
+│   └── ...                       # 制作过程中的一些笔记。
+├── src/
+│   ├── assets/             # 字体、样式等module 资源 (会被webpack处理)
+│   │   └── ...
+│   ├── components/     # 局部组件
+│   │   └── ...
+│   ├── data/                 # 模拟数据
+│   │   └── ...
+│   ├── pages/               # 主要页面组件
+│   │   └── ...
+│   ├── main.js                # app 入口文件
+│   ├── App.vue              # 主要app组件
+├── static/                      # 图片等纯静态资源
+├── test/
+│   └── unit/                   # unit tests
+│      ├── specs/              # test spec files
+│      ├── index.js            # test build entry file
+│      └── karma.conf.js       # test runner config file
+├── .babelrc                    # babel config
+├── .postcssrc.js               # postcss config
+├── .eslintrc.js                # eslint config
+├── .editorconfig               # editor config
+├── index.html                  # index.html template
+└── package.json                # build scripts and dependencies
+```
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,111 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+//
+// Vue.use(Router)
+//
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Hello',
+//       component: HelloWorld
+//     }
+//   ]
+// })
 
-Vue.use(Router)
 
-export default new Router({
-  routes: [
+
+
+import App from '../App'
+
+const home = r => require.ensure([], () => r(require('../pages/home/home')), 'home')
+const search = r => require.ensure([], () => r(require('../pages/search/search')), 'search')
+const msite = r => require.ensure([], () => r(require('../pages/msite/msite')), 'msite')
+
+
+const login = r => require.ensure([], () => r(require('../pages/login/login')), 'login')
+const profile = r => require.ensure([], () => r(require('../pages/profile/profile')), 'profile')
+const info = r => require.ensure([], () => r(require('../pages/profile/children/info')), 'info')
+const setusername = r => require.ensure([], () => r(require('../pages/profile/children/setusername')), 'setusername')
+const order = r => require.ensure([], () => r(require('../pages/order/order')), 'order')
+const service = r => require.ensure([], () => r(require('../pages/service/service')), 'service')
+const questionDetail = r => require.ensure([], () => r(require('../pages/service/children/questionDetail')), 'questionDetail')
+
+
+
+
+
+
+export default [{
+  path: '/',
+  component: App, //顶层路由，对应index.html
+  children: [ //二级路由。对应App.vue
+    //地址为空时跳转home页面
     {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      path: '',
+      redirect: '/home'
+    },
+    //首页城市列表页
+    {
+      path: '/home',
+      component: home
+    },
+    //当前选择城市页---->跳转到首页
+    {
+      path: '/city/:cityid',
+      redirect: '/msite'
+    },
+    //站点首页
+    {
+      path: '/msite',
+      component: msite,
+      meta: { keepAlive: true },
+    },
+    //搜索页
+    {
+      path: '/search/:geohash',
+      component: search,
+      meta: { keepAlive: true },
+    },
+    //登录注册页
+    {
+      path: '/login',
+      component: login
+    },
+    //个人信息页
+    {
+      path: '/profile',
+      component: profile,
+      children: [{
+        path: 'info', //个人信息详情页
+        component: info,
+
+      },
+        {
+          path: 'setusername',
+          component: setusername,
+        },
+        {
+          path: 'service', //服务中心
+          component: service,
+        },]
+    },
+    //服务中心
+    {
+      path: '/service',
+      component: service,
+      children: [{
+        path: 'questionDetail', //订单详情页
+        component: questionDetail,
+      }, ]
+    },
+    //订单列表页
+    {
+      path: '/order',
+      component: order
     }
+
+
   ]
-})
+}]
